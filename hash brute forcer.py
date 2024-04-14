68571566bc8777e96de1d04e77ccbfda42cfdee0
fd <98180503+fivesquaredzerofive@users.noreply.github.com>
2024-04-15 00:12:08 +0400

    Update the script to brute force sha512 too.

--- hash brute forcer.py
+++ hash brute forcer.py
@@ -7,13 +7,29 @@ def generate_strings(n):
         for combination in itertools.product(characters, repeat=i):
             yield ''.join(combination)
 
+def sha256bruteforce():
+    a = int(input("Input the length of the string: "))
+    g = input("Input the hash: ")
 
+    for string in generate_strings(a):
+        b = string.encode('utf-8')
+        if hashlib.sha256(b).hexdigest() == g:
+            print(string)
+            break;
 
-a = int(input("Input the length of the string: "))
-g = input("Input the hash: ")
+def sha512bruteforce():
+    a = int(input("Input the length of the string: "))
+    g = input("Input the hash: ")
 
-for string in generate_strings(a):
-    b = string.encode('utf-8')
-    if hashlib.sha256(b).hexdigest() == g:
-        print(string)
-        break;
+    for string in generate_strings(a):
+        b = string.encode('utf-8')
+        if hashlib.sha512(b).hexdigest() == g:
+            print(string)
+            break;
+
+quest = input("Do you want to brute force sha256 or sha512?(sha256/sha512): ")
+if quest == "sha256":
+    sha256bruteforce()
+elif quest == "sha512":
+    sha512bruteforce()
+
